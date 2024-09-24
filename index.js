ed4f4ca01983e6c449a40498c8af7261f3f9ddde
joyce wamocho <joywamo@gmail.com>
2024-09-24 13:48:39 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.round(Math.random()*10) + 1;
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(2, 5, 10, 15, 20);
